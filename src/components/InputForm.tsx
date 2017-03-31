bf9cafa507ccea47016d202cdaf2cb7a15cac1eb
Robert Smith <rsmith@sonovate.com>
2017-03-31 16:06:18 +0100

    added input state

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,8 +1,6 @@
 //Libs
 import * as React from "react";
 
-//Components
-
 //Stylesheets
 import "../stylesheets/components/InputForm.scss";
 
@@ -10,20 +8,44 @@ interface InputFormProps {
 
 }
 
-export default class InputForm extends React.Component<InputFormProps, undefined> {
+export interface InputFormState{
+    value: string;
+}
+
+export default class InputForm extends React.Component<InputFormProps, InputFormState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event: any) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event: UIEvent) {
+        //pass up event to state
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="InputForm">
                 <h1 className="InputForm__title">
                     Weather Application
                 </h1>
-                <form className="InputForm__form">
+                <form className="InputForm__form-container">
                     <label>
                         Postcode:
                         <input 
                             type="text" 
                             name="Postcode"
                             className="InputForm__input"
+                            value={this.state.value}
+                            onChange={this.handleChange}
                         />
                     </label>
                     <input 
